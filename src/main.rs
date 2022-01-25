3935d18f165eabd71ec983e3e00acd269322ed06
camila314 <camden7306@gmail.com>
2022-01-25 13:48:09 -0600

    some stuff

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
-fn main() {
-    let cmd = std::env::args().nth(1).expect("Help information for geode or smth");
+use std::path::PathBuf;
+use clap::Args;
+use colored::*;
+use clap::Parser;
+use clap::Subcommand;
+use path_absolutize::*;
 
-    println!("{}", cmd);
+#[derive(Parser)]
+#[clap(version, long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    New {
+        location: PathBuf,
+        name: Option<String>
+    },
+}
+fn main() {
+    let args = Cli::parse();
+    match args.command {
+        Commands::New { location, name } => {
+            let absolute_location = location.absolutize().unwrap();
+            let project_name = match name {
+                Some(s) => s,
+                None => absolute_location.file_name().unwrap().to_str().unwrap().to_string()
+            };
+            println!("insane {} name {}", absolute_location.to_str().unwrap().blue(), project_name);
+        }
+    }
 }
