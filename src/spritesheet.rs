709020f130eef6c998a203c29cf93a63dfba5e0c
camila314 <camden7306@gmail.com>
2022-01-31 10:43:39 -0600

    even better method

--- src/spritesheet.rs
+++ src/spritesheet.rs
@@ -39,7 +39,7 @@ struct GameSheet {
 pub fn pack_sprites(in_dir: &Path, out_dir: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let mut config = TexturePackerConfig {
         max_width: 0,
-        max_height: u32::MAX,
+        max_height: 0,
         allow_rotation: true,
         texture_outlines: false,
         border_padding: 1,
@@ -47,7 +47,6 @@ pub fn pack_sprites(in_dir: &Path, out_dir: &Path) -> Result<(), Box<dyn std::er
     };
 
     let mut frames = Vec::<(PathBuf, String)>::new();
-
     for walk in walkdir::WalkDir::new(in_dir) {
         let s = walk?;
 
@@ -70,9 +69,12 @@ pub fn pack_sprites(in_dir: &Path, out_dir: &Path) -> Result<(), Box<dyn std::er
         }
 
         config.max_width += dim.0;
+        if config.max_height < dim.1 {
+            config.max_height = dim.1;
+        }
     }
-
-    config.max_width /= (frames.len()/5) as u32;
+    config.max_width = (config.max_width as f64 * config.max_height as f64).sqrt() as u32;
+    config.max_height = u32::MAX;
 
     let mut packer = TexturePacker::new_skyline(config);
 
