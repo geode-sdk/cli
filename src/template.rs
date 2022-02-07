84151dc3802808e724cd74fc447573da37b4c987
matcool <26722564+matcool@users.noreply.github.com>
2022-02-06 22:10:28 -0300

    Clean up code, remove ide set up code

--- src/template.rs
+++ src/template.rs
@@ -6,14 +6,11 @@ use path_absolutize::Absolutize;
 use rustyline::Editor;
 use serde_json::{json, to_string_pretty};
 use std::path::PathBuf;
-use std::{fs, path::Path, process::exit};
-use std::process::Command;
-use std::env;
-use std::process;
+use std::{fs, path::Path};
 
 use fs_extra::dir as fs_dir;
 
-pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
+pub fn create_template(project_name: String, location: Option<PathBuf>) {
 	let is_location_default = location.is_none();
 	let loc = match location {
 	    Some(s) => s,
@@ -63,7 +60,6 @@ pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
 	buffer = buffer.trim().to_string();
 	version = version.trim().to_string();
 	developer = developer.trim().to_string();
-	project_name = project_name.trim().to_string();
 	description = description.trim().to_string();
 
 	let project_location = Path::new(&buffer);
@@ -83,19 +79,16 @@ pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
 	);
 
 	if project_location.exists() {
-	    println!("{}", "Unable to create project in existing directory".red());
-	    exit(1);
+	    print_error!("Unable to create project in existing directory");
 	} else {
-		let result = fs::create_dir_all(&project_location);
-		if result.is_err() {
-			println!("{}", "Unable to create directory for project".red());
-			exit(1);
+		if fs::create_dir_all(&project_location).is_err() {
+			print_error!("Unable to create directory for project");
 		}
 	}
 
 	match Repository::clone("https://github.com/geode-sdk/example-mod", &project_location) {
 	    Ok(_) => (),
-	    Err(e) => print_error!("failed to clone template: {}", e),
+	    Err(e) => print_error!("Failed to clone template: {}", e),
 	};
 
 	fs::remove_dir_all(&project_location.join(".git")).unwrap();
@@ -107,7 +100,7 @@ pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
 
 	        fs::write(file, contents).unwrap();
 	    }
-	}         
+	}
 
 	let tmp_sdk = std::env::temp_dir().join("sdk");
 
@@ -117,7 +110,7 @@ pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
 
 	match Repository::clone_recurse("https://github.com/geode-sdk/sdk", &tmp_sdk) {
 	    Ok(_) => (),
-	    Err(e) => print_error!("failed to clone sdk: {}", e),
+	    Err(e) => print_error!("Failed to clone sdk: {}", e),
 	};
 
 	copy_dir::copy_dir(&tmp_sdk, project_location.join("sdk")).unwrap();
@@ -148,49 +141,4 @@ pub fn create_template(mut project_name: String, location: Option<PathBuf>) {
 	    &project_location.join("mod.json"),
 	    to_string_pretty(&mod_json).unwrap()
 	).expect("Unable to write to specified project");
-	
-	let location_str = project_location.parent().unwrap().to_str().unwrap();
-
-	if cfg!(windows)
-	{
-		let mut set_up_project = String::new();
-		println!("Would you like to set up and open the project? (y/n):");
-		let _answer = std::io::stdin().read_line(&mut set_up_project).unwrap();
-
-		let mod_folder = format!("{}/{}", &location_str, &project_name);
-	
-		if set_up_project.trim() == "y"
-		{
-			let mut ide = String::new();
-			println!("Select Compatible IDE: \n1. VS Code.\n2. Visual Studio.");
-			let _ide_answer = std::io::stdin().read_line(&mut ide).unwrap();
-	
-			let build_folder_in_mod = format!("{}/build", &mod_folder);
-			
-			if ide.trim() == "1" // Open VS Code
-			{
-				assert!(env::set_current_dir(&mod_folder).is_ok());
-				Command::new("cmd").arg("/c").arg("code").arg(".").spawn().expect("Uh oh!");
-			}
-			else if ide.trim() == "2" // Open and Set Up Visual Studio
-			{
-				std::fs::create_dir(&build_folder_in_mod).unwrap();
-				assert!(env::set_current_dir(&build_folder_in_mod).is_ok());
-				//println!("Successfully changed working directory to {}!", env::current_dir().unwrap().into_os_string().into_string().unwrap());
-	
-				let mut cmake = Command::new("cmake").arg("..").arg("-A").arg("Win32").spawn().expect("Uh oh!");
-				let _end = cmake.wait().unwrap();
-				let sln_file = format!("{}.sln", project_name);
-				println!("Opening Visual Studio Solution...");
-				Command::new("cmd").arg("/c").arg(sln_file).spawn().expect("Uh oh!");
-			}
-		}
-		else if set_up_project.trim() == "n"
-		{
-			assert!(env::set_current_dir(&mod_folder).is_ok());
-			Command::new("explorer").arg(".").spawn().expect("Uh oh!");
-		}
-	}
-
-	process::exit(0);
 }
