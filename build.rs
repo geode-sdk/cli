a2a064de49d1ab5c0c3a369dbf4cab4eb6192f2f
fig <17240592+FigmentBoy@users.noreply.github.com>
2022-02-13 12:14:01 -0600

    No idea why this didn't work earlier

--- build.rs
+++ build.rs
@@ -1,7 +1,6 @@
-#[cfg(windows)] extern crate winres;
-
 fn main() {
   if cfg!(target_os = "windows") {
+    extern crate winres;
     let mut res = winres::WindowsResource::new();
     res.set_icon("geode.ico");
     res.compile().unwrap();
