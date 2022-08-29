975fd990310641664e9edbcb5ff93e0833edf215
HJfod <60038575+HJfod@users.noreply.github.com>
2022-08-29 12:04:55 +0300

    fix build on windows

--- build.rs
+++ build.rs
@@ -1,8 +1,8 @@
 #[cfg(windows)] extern crate winres;
 fn main() {
-  #[cfg(windows)] {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("geode.ico");
-    res.compile().unwrap();
-  }
+    #[cfg(windows)] {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("geode.ico");
+        res.compile().unwrap();
+    }
 }
