44564b467b4a8206385e7a3cc3c88f83c5b4dd06
HJfod <60038575+HJfod@users.noreply.github.com>
2022-10-11 16:10:11 +0300

    update sdk to use tags and heads instead of nightly branch + add `geode sdk install-binaries` command

--- src/sdk.rs
+++ src/sdk.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::borrow::Borrow;
 use std::io::{stdout, stdin, Write};
 use std::path::{PathBuf, Path};
 use crate::config::Config;
@@ -6,46 +6,37 @@ use clap::Subcommand;
 use git2::build::RepoBuilder;
 use git2::{FetchOptions, RemoteCallbacks, Repository};
 use colored::Colorize;
+use serde::{Deserialize};
 use std::fs;
+use semver::Version;
+use reqwest::header::{HeaderMap, USER_AGENT, HeaderValue};
 
-use crate::{fail, warn, info, done};
+use crate::{fail, warn, info, done, fatal};
 use crate::NiceUnwrap;
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct Version {
-	pub major: u32,
-	pub minor: u32,
-	pub patch: u32,
+#[derive(Deserialize)]
+struct GithubReleaseAsset {
+	name: String,
+	browser_download_url: String,
 }
 
-impl Version {
-	pub fn to_string(&self) -> String {
-		self.into()
-	}
+#[derive(Deserialize)]
+struct GithubReleaseResponse {
+	assets: Vec<GithubReleaseAsset>,
 }
 
-impl From<String> for Version {
-	fn from(str: String) -> Self {
-		let mut iter = str.split(".");
-		let (major, minor, patch) = (
-			iter.next().and_then(|n| n.parse::<u32>().ok()).nice_unwrap("Invalid major part in version"),
-			iter.next().and_then(|n| n.parse::<u32>().ok()).nice_unwrap("Invalid minor part in version"),
-			iter.next().and_then(|n| n.parse::<u32>().ok()).nice_unwrap("Invalid patch part in version")
-		);
-		Version { major, minor, patch }
-	}
+#[derive(clap::ValueEnum, Clone, Debug)]
+pub enum Branch {
+	Nightly,
+	Stable,
 }
 
-impl From<&Version> for String {
-	fn from(ver: &Version) -> Self {
-		format!("v{}.{}.{}", ver.major, ver.minor, ver.patch)
-	}
-}
-
-impl Display for Version {
-	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.write_fmt(format_args!("v{}.{}.{}", self.major, self.minor, self.patch))
-	}
+fn download_url(url: String, file_name: &PathBuf) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+	let res = reqwest::blocking::get(url)?;
+	let mut file = fs::File::create(file_name)?;
+	let mut content = std::io::Cursor::new(res.bytes()?);
+	std::io::copy(&mut content, &mut file)?;
+	Ok(())
 }
 
 #[derive(Subcommand, Debug)]
@@ -60,21 +51,29 @@ pub enum Sdk {
 		path: Option<PathBuf>
 	},
 
+	/// Install prebuilt binaries for SDK
+	InstallBinaries,
+
 	/// Uninstall SDK
 	Uninstall,
 
 	/// Update SDK
 	Update {
-		/// Set update branch to nightly
-		nightly: bool,
-
-		/// Set update branch to stable
-		#[clap(conflicts_with("nightly"))]
-		stable: bool
+		/// Set update branch
+		#[clap(value_enum)]
+		branch: Option<Branch>,
 	},
 
 	/// Get SDK version
 	Version,
+}
+
+fn parse_version(str: &str) -> Result<Version, semver::Error> {
+	if str.starts_with("v") {
+		Version::parse(&str[1..])
+	} else {
+		Version::parse(str)
+	}
 }
 
 fn uninstall(config: &mut Config) -> bool {
@@ -136,28 +135,37 @@ fn install(config: &mut Config, path: PathBuf) {
 		let mut fetch = FetchOptions::new();
 		fetch.remote_callbacks(callbacks);
 
-		let mut repo = RepoBuilder::new();
-		repo.fetch_options(fetch);
+		let mut builder = RepoBuilder::new();
+		builder.fetch_options(fetch);
 
+		let repo = builder.clone("https://github.com/geode-sdk/geode", &path)
+			.nice_unwrap("Could not download SDK");
 
-		repo.clone("https://github.com/geode-sdk/geode", &path).nice_unwrap("Could not download SDK");
-		
 		config.sdk_path = Some(path);
+
+		switch_to_tag(config, &repo);
+
 		done!("Successfully installed SDK");
+		info!("Use `geode sdk install-binaries` to install pre-built binaries");
 	}
 }
 
-fn update(config: &mut Config, nightly: bool, stable: bool) {
+fn update(config: &mut Config, branch: Option<Branch>) {
 	// Switch branch if necessary
-	if nightly && stable {
-		unreachable!("Contact geode developers");
-	} else if nightly {
-		config.sdk_nightly = true;
-	} else if stable {
-		config.sdk_nightly = false;
-	}
-	let branch = config.sdk_nightly.then_some("nightly").unwrap_or("main");
+	match branch {
+		Some(Branch::Nightly) => {
+			info!("Switching to nightly");
+			config.sdk_nightly = true;
+		},
+		Some(Branch::Stable) => {
+			info!("Switching to stable");
+			config.sdk_nightly = false;
+		},
+		None => {},
+	};
 
+	info!("Updating SDK");
+	
 	// Initialize repository
 	let repo = Repository::open(
 		config.sdk_path.as_ref().nice_unwrap("Unable to update SDK as it is not installed")
@@ -175,7 +183,7 @@ fn update(config: &mut Config, nightly: bool, stable: bool) {
 	});
 
 	remote.fetch(
-		&[branch],  
+		&["main"],  
 		Some(FetchOptions::new().remote_callbacks(callbacks)),
 		None
 	).nice_unwrap("Could not fetch latest update");
@@ -187,28 +195,135 @@ fn update(config: &mut Config, nightly: bool, stable: bool) {
 	let merge_analysis = repo.merge_analysis(&[&fetch_commit]).unwrap().0;
 
 	if merge_analysis.is_up_to_date() {
+		switch_to_tag(config, &repo);
+
 		done!("SDK is up to date");
 	} else if !merge_analysis.is_fast_forward() {
 		fail!("Cannot update SDK, it has local changes");
-		info!("Help: go into the repository and manually do `git pull`");
+		info!("Go into the repository at {} and manually run `git pull`",
+			config.sdk_path.as_ref().unwrap().to_str().unwrap()
+		);
 	} else {
 		// Change head and checkout
-		let refname = format!("refs/heads/{}", branch);
-		let mut reference = repo.find_reference(&refname).unwrap();
-		reference.set_target(fetch_commit.id(), "Fast-Forward").unwrap();
-		repo.set_head(&refname).unwrap();
-		repo.checkout_head(None).unwrap();
+
+		switch_to_tag(config, &repo);
 
 		done!("Successfully updated SDK.");
 	}
 }
 
+fn switch_to_tag(config: &mut Config, repo: &Repository) {
+	info!("Updating head");
+
+	if config.sdk_nightly {
+		let fetch_head = repo.find_reference("FETCH_HEAD").unwrap();
+		let fetch_commit = repo.reference_to_annotated_commit(&fetch_head).unwrap();
+		repo.set_head("refs/heads/main").unwrap();
+		let mut refer = repo.find_reference("refs/heads/main").unwrap();
+		refer.set_target(fetch_commit.id(), "Fast-Forward").unwrap();
+		repo.checkout_head(None).nice_unwrap("Unable to switch to latest commit");
+		info!("Switched to latest commit");
+		return;
+	}
+
+	let mut latest_version: Option<Version> = None;
+	for tag in repo.tag_names(None).nice_unwrap("Unable to get SDK tags").iter() {
+		if let Some(tag) = tag {
+			if let Ok(version) = parse_version(tag) {
+				if latest_version.as_ref().is_none() {
+					latest_version = Some(version);
+				} else if version > latest_version.as_ref().unwrap().to_owned() {
+					latest_version = Some(version);
+				}
+			}
+		}
+	}
+
+	if latest_version.is_none() {
+		warn!("No SDK tags found, unable to switch");
+		return;
+	}
+
+	// Change head and checkout
+	let refname = format!("refs/tags/v{}", latest_version.unwrap());
+	repo.set_head(&refname).unwrap();
+	repo.checkout_head(None).unwrap();
+
+	done!("Updated head");
+}
+
+fn install_binaries(config: &mut Config) {
+	config.sdk_path.as_ref().nice_unwrap(
+		"SDK not installed! Use `geode sdk install` to install \
+		Geode or `geode config setup` to set up the CLI."
+	);
+	update(config, None);
+	let release_tag: String;
+	let target_dir: PathBuf;
+	if config.sdk_nightly {
+		info!("Installing nightly binaries");
+		release_tag = "Nightly".into();
+		target_dir = config.sdk_path.as_ref().unwrap().join("bin/nightly");
+	} else {
+		let ver = get_version(config);
+		info!("Installing binaries for {}", ver);
+		release_tag = format!("v{}", ver);
+		target_dir = config.sdk_path.as_ref().unwrap().join(format!("bin/{}", ver));
+	}
+	let url = format!("https://api.github.com/repos/geode-sdk/geode/releases/tags/{}", release_tag);
+
+	let mut headers = HeaderMap::new();
+	headers.insert(USER_AGENT, HeaderValue::from_static("github_api/1.0"));
+
+	let res = reqwest::blocking::Client::new()
+		.get(&url).headers(headers).send()
+		.nice_unwrap("Unable to get download info from GitHub")
+		.json::<GithubReleaseResponse>()
+		.nice_unwrap("Unable to parse GitHub response");
+	
+	let mut target_url: Option<String> = None;
+	for asset in res.assets {
+		#[cfg(target_os = "windows")]
+		if asset.name.to_lowercase().contains("win") {
+			target_url = Some(asset.browser_download_url);
+			info!("Found binaries for platform Windows");
+			break;
+		}
+
+		#[cfg(target_os = "macos")]
+		if asset.name.to_lowercase().contains("mac") {
+			target_url = Some(asset.browser_download_url);
+			info!("Found binaries for platform MacOS");
+			break;
+		}
+	}
+
+	if target_url.is_none() {
+		fatal!("No binaries found for current platform");
+	}
+
+	fs::create_dir_all(&target_dir).nice_unwrap("Unable to create directory for binaries");
+
+	info!("Downloading");
+
+	let temp_zip = target_dir.join("temp.zip");
+	download_url(target_url.unwrap(), &temp_zip).nice_unwrap("Downloading binaries failed");
+
+	let file = fs::File::open(&temp_zip).nice_unwrap("Unable to read downloaded ZIP");
+	let mut zip = zip::ZipArchive::new(file).nice_unwrap("Downloaded ZIP appears to be corrupted");
+	zip.extract(target_dir).nice_unwrap("Unable to unzip downloaded binaries");
+	
+	fs::remove_file(temp_zip).nice_unwrap("Unable to clean up downloaded ZIP");
+
+	done!("Binaries installed");
+}
+
 pub fn get_version(config: &mut Config) -> Version {
-	Version::from(
+	Version::parse(
 		fs::read_to_string(
 			config.sdk_path.as_ref().nice_unwrap("SDK not installed!").join("VERSION")
-		).nice_unwrap("Unable to read SDK version, make sure you are using SDK v0.4.2 or later")
-	)
+		).nice_unwrap("Unable to read SDK version, make sure you are using SDK v0.4.2 or later").as_str()
+	).nice_unwrap("Invalid SDK version")
 }
 
 pub fn subcommand(config: &mut Config, cmd: Sdk) {
@@ -229,7 +344,8 @@ pub fn subcommand(config: &mut Config, cmd: Sdk) {
 			install(config, path.unwrap_or(default_path));
 		},
 		Sdk::Uninstall => { uninstall(config); },
-		Sdk::Update { nightly, stable } => update(config, nightly, stable),
-		Sdk::Version => info!("Geode SDK version: {}", get_version(config))
+		Sdk::Update { branch } => update(config, branch),
+		Sdk::Version => info!("Geode SDK version: {}", get_version(config)),
+		Sdk::InstallBinaries => install_binaries(config),
 	}
 }
