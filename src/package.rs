448289ed461878126ae5ddaa0a16f8f02aef141d
rooot <hey@rooot.gay>
2024-04-22 17:27:24 +0200

    add .ios.dylib to potential_names
    Signed-off-by: rooot <hey@rooot.gay>

--- src/package.rs
+++ src/package.rs
@@ -435,7 +435,7 @@ fn merge_packages(inputs: Vec<PathBuf>) {
 		.nice_unwrap("Unable to create zip writer");
 
 	for archive in &mut archives {
-		let potential_names = [".dylib", ".so", ".dll", ".lib"];
+		let potential_names = [".ios.dylib", ".dylib", ".so", ".dll", ".lib"];
 
 		// Rust borrow checker lol xd
 		let files: Vec<_> = archive.file_names().map(|x| x.to_string()).collect();
