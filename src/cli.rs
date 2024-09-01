4f5889ded045935ddb20bf9c3ecf7eb5375a932e
bradinator <123034682+Ulladay-Hub@users.noreply.github.com>
2024-09-01 13:02:19 +1000

    Add links to refer to weblinks.

--- src/cli.rs
+++ src/cli.rs
@@ -4,99 +4,105 @@ use crate::mod_file::PlatformName;
 #[derive(clap::Parser, Debug)]
 #[clap(version)]
 pub struct Args {
-	#[clap(subcommand)]
-	pub command: GeodeCommands,
+    #[clap(subcommand)]
+    pub command: GeodeCommands,
 }
 
 #[derive(clap::Subcommand, Debug)]
 pub enum GeodeCommands {
-	/// Initialize a new Geode project
-	New {
-		/// The target directory to create the project in
-		path: Option<std::path::PathBuf>,
-	},
+    /// Initialize a new Geode project
+    New {
+        /// The target directory to create the project in
+        path: Option<std::path::PathBuf>,
+    },
 
-	/// Generate shell completions and print it to stdout
-	Completions { shell: clap_complete::Shell },
+    /// Generate shell completions and print it to stdout
+    Completions { shell: clap_complete::Shell },
 
-	/// Generate manpage and print it to stdout
-	GenerateManpage {},
+    /// Generate manpage and print it to stdout
+    GenerateManpage {},
 
-	/// Options for managing profiles (installations of Geode)
-	Profile {
-		#[clap(subcommand)]
-		commands: crate::profile::Profile,
-	},
+    /// Options for managing profiles (installations of Geode)
+    Profile {
+        #[clap(subcommand)]
+        commands: crate::profile::Profile,
+    },
 
-	/// Options for configuring Geode CLI
-	Config {
-		#[clap(subcommand)]
-		commands: crate::info::Info,
-	},
+    /// Options for configuring Geode CLI
+    Config {
+        #[clap(subcommand)]
+        commands: crate::info::Info,
+    },
 
-	/// Options for installing & managing the Geode SDK
-	Sdk {
-		#[clap(subcommand)]
-		commands: crate::sdk::Sdk,
-	},
+    /// Options for installing & managing the Geode SDK
+    Sdk {
+        #[clap(subcommand)]
+        commands: crate::sdk::Sdk,
+    },
 
-	/// Tools for working with the current mod project
-	Project {
-		#[clap(subcommand)]
-		commands: crate::project::Project,
-	},
+    /// Options for managing links
+    Links {
+        #[clap(subcommand)]
+        commands: crate::links::Links,
+    },
 
-	/// Options for working with .geode packages
-	Package {
-		#[clap(subcommand)]
-		commands: crate::package::Package,
-	},
+    /// Tools for working with the current mod project
+    Project {
+        #[clap(subcommand)]
+        commands: crate::project::Project,
+    },
 
-	/// Tools for interacting with the Geode mod index
-	Index {
-		#[clap(subcommand)]
-		commands: crate::index::Index,
-	},
+    /// Options for working with .geode packages
+    Package {
+        #[clap(subcommand)]
+        commands: crate::package::Package,
+    },
 
-	/// Run default instance of Geometry Dash
-	Run {
-		/// Run Geometry Dash in the background instead of the foreground
-		#[clap(long, conflicts_with = "stay")]
-		background: bool,
+    /// Tools for interacting with the Geode mod index
+    Index {
+        #[clap(subcommand)]
+        commands: crate::index::Index,
+    },
 
-		/// Do not exit CLI after Geometry Dash exits if running in foreground
-		#[clap(long, conflicts_with = "background")]
-		stay: bool,
+    /// Run default instance of Geometry Dash
+    Run {
+        /// Run Geometry Dash in the background instead of the foreground
+        #[clap(long, conflicts_with = "stay")]
+        background: bool,
 
-		/// Launch arguments for Geometry Dash
-		#[clap(last = true, allow_hyphen_values = true)]
-		launch_args: Vec<String>,
-	},
+        /// Do not exit CLI after Geometry Dash exits if running in foreground
+        #[clap(long, conflicts_with = "background")]
+        stay: bool,
 
-	/// Builds the project at the current directory
-	Build {
-		/// Which platform to cross-compile to, if possible
-		#[clap(long, short)]
-		platform: Option<PlatformName>,
+        /// Launch arguments for Geometry Dash
+        #[clap(last = true, allow_hyphen_values = true)]
+        launch_args: Vec<String>,
+    },
 
-		/// Whether to only configure cmake
-		#[clap(long, short, default_value_t = false)]
-		configure_only: bool,
+    /// Builds the project at the current directory
+    Build {
+        /// Which platform to cross-compile to, if possible
+        #[clap(long, short)]
+        platform: Option<PlatformName>,
 
-		/// Whether to only build project
-		#[clap(long, short, default_value_t = false)]
-		build_only: bool,
+        /// Whether to only configure cmake
+        #[clap(long, short, default_value_t = false)]
+        configure_only: bool,
 
-		/// Android NDK path, uses ANDROID_NDK_ROOT env var otherwise
-		#[clap(long)]
-		ndk: Option<String>,
+        /// Whether to only build project
+        #[clap(long, short, default_value_t = false)]
+        build_only: bool,
 
-		/// Sets the cmake build type, defaults to Debug or RelWithDebInfo depending on platform
-		#[clap(long)]
-		config: Option<String>,
+        /// Android NDK path, uses ANDROID_NDK_ROOT env var otherwise
+        #[clap(long)]
+        ndk: Option<String>,
 
-		/// Extra cmake arguments when configuring
-		#[clap(last = true, allow_hyphen_values = true)]
-		extra_conf_args: Vec<String>,
-	},
+        /// Sets the cmake build type, defaults to Debug or RelWithDebInfo depending on platform
+        #[clap(long)]
+        config: Option<String>,
+
+        /// Extra cmake arguments when configuring
+        #[clap(last = true, allow_hyphen_values = true)]
+        extra_conf_args: Vec<String>,
+    },
 }
